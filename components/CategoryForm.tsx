60a3089fa2f8364ad72aa934f1a4022d07791ebe
Luandro <luandro@digital-democracy.org>
2023-08-28 07:23:17 -0300

    Added a new CategoryForm component and imported it in MapeoRender.

---
+++ components/CategoryForm.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+interface CategoryFormProps {
+  icon: string;
+  name: string;
+  borderColor: string;
+  sortValues: string[];
+  onSave: (data: {icon: string, name: string, borderColor: string, sortValues: string[]}) => void;
+}
+
+const CategoryForm: React.FC<CategoryFormProps> = ({ icon, name, borderColor, sortValues, onSave }) => {
+  const [formState, setFormState] = useState({ icon, name, borderColor, sortValues });
+
+  const handleChange = (event) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSave(formState);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="p-4 bg-white rounded shadow-md">
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="icon">
+          Icon
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="icon"
+          type="text"
+          name="icon"
+          value={formState.icon}
+          onChange={handleChange}
+        />
+      </div>
+      {/* Add similar input fields for name, borderColor, and sortValues */}
+      <div className="flex items-center justify-between">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          Save
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default CategoryForm;
--- components/MapeoRender.tsx
+++ components/MapeoRender.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
-import styles from './styles.module.css'
+import React, { useState, useEffect } from 'react';
+import styles from './styles.module.css';
+import CategoryForm from './CategoryForm';
 
 interface Preset {
 	name: string;
