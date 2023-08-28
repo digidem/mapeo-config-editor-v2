ac40bd595659da4d50f56b4d2c578e8fc948c7ca
Luandro <luandro@digital-democracy.org>
2023-08-27 22:53:48 -0300

    chore(package.json): bump version from 0.3.0 to 0.3.1 fix(api/build/[...id].ts): read metadata.json file and extract name and version to use in the response feat(api/build/[...id].ts): increment the minor version in the metadata.json file before building and write it back fix(build.tsx): update state object to include name and version from the response in the fetch call refactor(build.tsx): restructure the layout and content of the Build component

--- pages/api/build/[...id].ts
+++ pages/api/build/[...id].ts
@@ -10,22 +10,43 @@ const handler = async (
 		status: string | string[];
 		build: string;
 		error: string | null;
+		name: string;
+		version: string;
 	}>
 ) => {
 	const id = Array.isArray(req.query.id) ? req.query.id[0] : req.query.id;
-	console.log('GOT ID', id)
+	let metadata = {
+		name: 'unknown',
+		version: 'v0.0.1'
+	}
 	if (!id) {
 		throw ('Must pass ID')
 	}
 	const { outputDir } = getOutputDir(id)
 	const buildDir = path.join(outputDir, 'build')
+	try {
+		metadata = JSON.parse(fs.readFileSync(path.join(outputDir, 'metadata.json'), 'utf8'));
+	} catch (err) {
+		console.error('Error reading metadata.json:', err);
+	}
+	const { name, version } = metadata;
+
+	const outputFile = `${buildDir}/${name}-${version}.mapeosettings`
 	if (req.method == "POST") {
 		try {
 			// runs build on it
 			if (!fs.existsSync(buildDir)){
 				fs.mkdirSync(buildDir);
 			}
-			const build = await settingsBuilder({ output: buildDir }, outputDir)
+			// Bump the version in the metadata.json file
+			let versionParts = metadata.version.split('.');
+			let minorVersion = parseInt(versionParts[1]);
+			minorVersion++;
+			versionParts[1] = minorVersion.toString();
+			metadata.version = versionParts.join('.');
+			fs.writeFileSync(path.join(outputDir, 'metadata.json'), JSON.stringify(metadata, null, 2), 'utf8');
+
+			const build = await settingsBuilder({ output: outputFile }, outputDir)
 			console.log('BUILD!!', build)
 		} catch (err) {
 			console.error(err)
@@ -35,20 +56,21 @@ const handler = async (
 			const settingsFiles = fs.readdirSync(buildDir).filter(fn => fn.endsWith('.mapeosettings'));
 			if (settingsFiles.length > 0) {
 				const filePath = path.join(buildDir, settingsFiles[0]);
-				res.status(200).json({ status: 'done', build: filePath, error: null });
+				res.status(200).json({ status: 'done', build: filePath, name, version, error: null,  });
 			} else if (fs.existsSync(buildDir)) {
-				res.status(200).json({ status: 'building', build: '', error: null });
+				res.status(200).json({ status: 'building', build: '', error: null, name, version });
 			} else {
-				res.status(500).json({ status: 'error', build: '', error: 'Build directory does not exist' });
+				res.status(500).json({ status: 'error', build: '', error: 'Build directory does not exist', name, version });
 			}
 		} catch (err) {
-			res.status(500).json({ status: 'error', build: '', error: (err as Error).toString() });		
+			res.status(500).json({ status: 'error', build: '', error: (err as Error).toString(), name: name, version });		
 		}
 	}
 
 }
 
 export default handler;
+
 
 
 
--- pages/build.tsx
+++ pages/build.tsx
@@ -4,8 +4,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 const Build: NextPage = () => {
-	const [build, setBuild] = useState<string | null>(null);
-	const [status, setStatus] = useState<string | null>(null);
+	const [state, setState] = useState<{ build: string | null, status: string | null, name: string | null, version: string | null, error: any }>({ build: null, status: null, name: null, version: null, error: null });
 
 	const router = useRouter()
 	const id = Array.isArray(router.query?.id) ? router.query?.id[0] : router.query?.id;
@@ -15,11 +14,11 @@ const Build: NextPage = () => {
 			if (id) {
 				try {
 					const response = await fetch(`/api/build/${id}`)
-					const { build, status } = await response.json()
-					setStatus(status)
-					if (build) setBuild(build)
+					const { build, status, name, version } = await response.json()
+					setState(prevState => ({ ...prevState, build, status, name, version }))
 					console.log('Build response:', status)
 				} catch (error) {
+					setState(prevState => ({ ...prevState, error }))
 					console.error('Error building:', error)
 				}
 			}
@@ -27,6 +26,7 @@ const Build: NextPage = () => {
 		const interval = setInterval(() => checkBuild(), 3000);
 		return () => clearInterval(interval); // cleanup on component unmount
 	}, [id]);
+	const { build, status, name, version, error } = state
 	return (
 		<div>
 			<Head>
@@ -34,36 +34,26 @@ const Build: NextPage = () => {
 				<meta name="description" content="File uploader" />
 			</Head>
 
-			<div className="w-full max-w-3xl px-3 mx-auto">
-				<h1 className="mb-10 text-3xl font-bold text-gray-900">
-					Mapeo Configuration Editor
-				</h1>
-
-				<div className="space-y-10 bg-gray-100 p-5 rounded-lg">
-					<div>
-						<div className="flex items-center justify-center h-screen">
-							<main className="py-10 flex justify-between space-x-4">
-								<div className='flex flex-col items-center justify-center space-y-12'>
-									{build ? 
-										<a download href={build} className="flex flex-col items-center justify-center h-full">
-											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-12 w-12 animate-bounce mb-2">
-												<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-											</svg>
-											<span className="mt-8 px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 transition-colors duration-200">Download Build</span> 
-										</a>
-										: 
-										<div className="flex items-center flex-col justify-center space-x-3">
-											<div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
-											<div className="pt-12 text-lg text-gray-700 animate-pulse">{status || 'Loading...'}</div>
-										</div>
-									}
-									<div>
-										<button className="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700" onClick={() => reset()}>Restart</button>
-									</div>
-								</div>
-							</main>
-						</div>
-					</div>
+			<div className="w-full max-w-3xl px-3 mx-auto mt-12">
+				<div className="flex flex-col items-center justify-center space-y-10 bg-gray-100 p-5 rounded-lg py-48">
+					<main className="space-y-12 flex flex-col items-center justify-center">
+						<h2 className="text-4xl font-bold text-center">{name}</h2>
+						<p className="text-lg text-gray-500 text-center">{version}</p>
+						{build ? 
+							<a download href={build} className="flex flex-col items-center justify-center space-y-4">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-12 w-12 animate-bounce">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+								</svg>
+								<span className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 transition-colors duration-200">Download Build</span> 
+							</a>
+							: 
+							<div className="flex flex-col items-center justify-center space-y-3">
+								<div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+								<p className="pt-12 text-lg text-gray-700 animate-pulse">{status || 'Loading...'}</p>
+							</div>
+						}
+						<button className="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700" onClick={() => reset()}>Restart</button>
+					</main>
 				</div>
 			</div>
 			<footer>
